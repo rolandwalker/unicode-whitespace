768ad6e5e553fb3247b626fe6701d4b817ce1f92
Roland Walker <walker@pobox.com>
2012-09-01 17:19:07 -0400

    header fmt for package managers

--- unicode-whitespace.el
+++ unicode-whitespace.el
@@ -2,7 +2,7 @@
 ;;
 ;; Copyright (c) 2012 Roland Walker
 ;;
-;; Author: Roland Walker walker@pobox.com
+;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/unicode-whitespace
 ;; URL: http://raw.github.com/rolandwalker/unicode-whitespace/master/unicode-whitespace.el
 ;; Version: 0.2.1
