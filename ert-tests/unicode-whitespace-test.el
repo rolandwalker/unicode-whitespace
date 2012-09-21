6e639fd31dc793c0091499cdf3d476c22e9123e7
Roland Walker <walker@pobox.com>
2012-09-21 15:48:43 -0400

    include build and autoload with test target add stub test file no individual tests defined, though

---
+++ ert-tests/unicode-whitespace-test.el
@@ -0,0 +1,31 @@
+
+;; requires and setup
+
+(when load-file-name
+  (setq pcache-directory (expand-file-name "pcache/" (file-name-directory load-file-name)))
+  (setq package-enable-at-startup nil)
+  (setq package-load-list '((pcache t)
+                            (persistent-soft t)
+                            (ucs-utils t)))
+  (when (fboundp 'package-initialize)
+    (package-initialize)))
+
+(require 'persistent-soft)
+(require 'ucs-utils)
+(require 'unicode-whitespace)
+
+;;; this is a stub - no tests defined
+
+;;
+;; Emacs
+;;
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; mangle-whitespace: t
+;; require-final-newline: t
+;; coding: utf-8
+;; byte-compile-warnings: (not cl-functions)
+;; End:
+;;
+
+;;; unicode-whitespace-test.el ends here
